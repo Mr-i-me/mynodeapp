bda93bae168a64e38b9d58c14644c5e2b52ff87a
lucas souza <ibmstemp@gmail.com>
2019-08-21 19:04:42 -0300

    g

---
+++ Contract.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var ContractSchema = new mongoose.Schema({
+  stage_1: {
+    name: [ String, required: true]
+    email: String,
+    cpf: String,
+    valor: Number,
+    renda: String,
+    nascimento: String,
+    EstadoCivil: String,
+    Endereco: String
+  },
+  stage_2:{
+    cnhCpf: String,
+    comprovanteRenda: String,
+    imóvel: String
+
+  },
+  stage_3
+  status: String,
+  enum: ['pending', 'aproved', 'rejected']
+
+});
+mongoose.model('Contract', ContractSchema);
+module.exports = mongoose.model('Contract');
