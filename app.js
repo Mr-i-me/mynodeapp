019e956ba5fbc23ca69c9458592de8c42c9be089
lucas souza <ibmstemp@gmail.com>
2019-08-21 19:19:51 -0300

    last

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var app = express();
 var db = require('./database.js');
+var UserController = require('./controllers/UserController');
+app.use('/contracts', UserController);
 module.exports = app;
