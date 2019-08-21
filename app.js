aedacc820193430ac47a6e0d96b12de43b753e2f
gabriel schiavo <gabriel@lewagon.org>
2019-08-21 19:39:20 -0300

    import contract model in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var db = require('./database.js');
+var Contract = require('./models/Contract');
 var ContractController = require('./controllers/ContractController');
 app.use('/contracts', ContractController);
 module.exports = app;
