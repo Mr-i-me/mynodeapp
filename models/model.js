7054f86e1c6c3f6b094d65a867e9f169e6301cc3
gabriel schiavo <gabriel@lewagon.org>
2019-08-22 12:57:13 -0300

    add some routes in api file

--- models/model.js
+++ models/model.js
@@ -28,18 +28,43 @@ let UserSchema = new Schema({
   nome: {
     type: String,
     required: [true, 'Name field is required']
+    default: ""
   },
-  rank: {
+  email: {
     type: String
+    required [true, 'Email field is required'],
+    default: ""
   },
-  available: {
-    type: Boolean,
-    default: false
+  cpf: {
+    type: String,
+    required [true, 'CPF field is required'],
+    default: ""
   },
-  geometry: GeoSchema
-
+  valorEmprestimo: {
+    type: Number,
+    required [true, 'Valor do Emprestimo field is required'],
+    default: 0
+  },
+  rendaMensal: {
+    type: Number,
+    default: 0
+  },
+  dataNascimento: {
+    type: Date,
+    default: Date.now
+  },
+  estadoCivil: {
+    type: ['solteiro(a)', 'casado(a)', 'divorciado(a)']
+    default: undefined
+  },
+  endereco: {
+    type: String,
+    default: ""
+  },
+  photos: UploadSchema,
+  approved: CheckoutSchema
 });
 
 
-mongoose.model('Contract', ContractSchema);
-module.exports = mongoose.model('Contract');
+const User = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User');
---
+++ routes/api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const User = require('../models/model');
+
+let router = express.Router();
+
+//Add user to db
+router.post('/users', function (req, res, next) {
+  User.create(req.body).then(function (data) {
+    res.send(data);
+  }).catch(next);
+});
+
+//Update data in the db
+router.put('/users/:id', function (req, res, next) {
+  User.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
+    User.findOne({ _id: req.params.id }).then(function (user) {
+      res.send(user);
+    });
+    res.send(user);
+  });
+});
+
+//Delete data in the db
+router.delete('/users/:id', function (req, res, next) {
+  User.findByIdAndRemove({_id: req.params.id }).then(function (user) {
+    res.send(user);
+  });
+});
+
+//Export module to access it in other module
+module.exports = router;
